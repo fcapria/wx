f6e3ea8c70450ad33e8374d4d1c74c4a85eda0a2
Frank Capria <fcapria@gmail.com>
2019-12-22 12:47:33 -0500

    degrees2dir moved to wx_conversions

---
+++ wx_conversions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Both functions a string, not a float or int
+# Ready for insertion into Sheet cell
+
+def toMph(kn):
+    mph = kn * 1.151
+    mph = str(int(round(mph,0))) + ' MPH'
+    return mph
+
+def toMi(nmi):
+    mi = nmi *1.151
+    mi = strint((round(mi,0))) + ' MPH'
+    return(mi)
+
+def degrees2dir(d):
+    # This is highly approximate but good enough for now
+    dirs = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE',
+            'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW']
+    ix = int((d + 11.25)/22.5)
+    return dirs[ix % 16]
