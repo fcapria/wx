e1652dea2e683d421a03b700ca1fb8f95cfc4336
Frank Capria <fcapria@gmail.com>
2019-12-30 15:07:44 -0500

    Publishes to Google Sheet

--- hi_low_avg.py
+++ hi_low_avg.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import json, time
+# This script is designed to be run every 15 minutes to update today's data
+# and archive the readings.
+
+# Yesterday's data is only updated if it has not been archived and the day 
+# is complete.
+
+import json, gspread
+from datetime import datetime
+from oauth2client.service_account import ServiceAccountCredentials 
 from datetime import datetime
 from tinydb import TinyDB, Query, where
 from statistics import mean
@@ -14,24 +22,26 @@ def up_to_date(yesterday):
     if len(result) > 0:
         current = True
     archive.close()
-    print(current)
     return current
 
 def calc_yesterday(yesterday):
-    print('Entered calc_yesterday')
     db = TinyDB('db.json')
     temps = []
     Date = Query()
     result = db.search(Date.date == yesterday)
     for item in result:
         temps.append(item['temp'])
-    print(temps)
     db.close()
     high = round(max(temps),1)
     low = round(min(temps),1)
     avg = round(mean(temps),1)
     archive = TinyDB('archive.json')    
     archive.insert({'date': yesterday, 'high': high, 'low': low, 'avg': avg})
+
+def make_str(n):
+    suffix = '°F'
+    n = str(n) + suffix
+    return n
 
 # Get today's date as an integer
 currently = datetime.now()
@@ -44,29 +54,41 @@ if not current:
 
 # Query for temps from today and yesterday and load into lists
 db = TinyDB("db.json")
-yesterdayTemps = []
 todayTemps = []
 for item in db:
     if item['date'] == today:
         todayTemps.append(item['temp'])
-    elif item['date'] == yesterday:
-        yesterdayTemps.append(item['temp'])
 
-# Calculate high, low and average for each day
+# Calculate high, low and average for today
 # Mean is from statistics
 # Min and max are built into Python
 
 todayHigh = round(max(todayTemps),1)
 todayLow = round(min(todayTemps),1)
 todayAvg = round(mean(todayTemps),1)
-print('Today')
-print(todayHigh, todayLow, todayAvg)
 
-archive = TinyDB('archive.json')
-Date = Query()
-result = archive.search(Date.date == yesterday)
-# There can only be one entry per date
-print('Yesterday')
-print(result[0]['high'],result[0]['low'],result[0]['avg'])
+# Publish today's data only
 
-# Publish them somewhere
+# Use credentials to create a client to interact with the Google Drive API
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('wx_secret.json', scope)
+client = gspread.authorize(creds)
+
+# Access workbook by name and open the appropriate tab
+
+try:
+    sheet = client.open('wx04849').get_worksheet(1)
+    # 1 is the index value of Sheet2
+    # Google doesn't document how to access Sheet2
+    # See https://stackoverflow.com/questions/33570749/python-gspread-library-only-writes-to-worksheet-labeled-sheet1
+except:
+    print ("Sheet didn't open when called by yesterday.py")
+
+sheet.update_cell(6,2,make_str(todayHigh))
+sheet.update_cell(7,2,make_str(todayLow))
+sheet.update_cell(8,2,make_str(todayAvg))
+
+stamp = str(datetime.now())
+sheet.update_cell(6,3,stamp)
+
+print("Today's high, low and averages updated.")
