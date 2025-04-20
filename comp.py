b68a83358c6150d1800800c5076d72b0e109cb19
Frank Capria <fcapria@gmail.com>
2025-04-20 13:39:38 -0400

    Optimize comp.py with batch update for daylight comparison row

--- comp.py
+++ comp.py
@@ -9,40 +9,53 @@ github.com/fcapria/wx
 """
 
 from datetime import datetime
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+import gspread, logging, json
+from google.oauth2.service_account import Credentials
 from wx_conversions import getSun
 from os import path
- 
+from wx_util import initLogging
+
 # body begins
+initLogging("comp.py")
 
 filePath = path.abspath(__file__) # full path of this script
 dirPath = path.dirname(filePath) # full path of the directory 
 jsonFilePath = path.join(dirPath,'wx_secret.json') # absolute json file path
 
-mckay = "lat=44.31&lng=-69.05"
+
+# Load latitude and longitude for ME from sunlight_config.json
+sunlightPath = path.join(dirPath, 'sunlight_config.json')
+with open(sunlightPath) as f:
+    sunlightConfig = json.load(f)
+    lat = float(sunlightConfig['latitude'])
+    lon = float(sunlightConfig['longitude'])
+    mckay = f"lat={lat:.2f}&lng={lon:.2f}"
+
+# Load latitude and longitude for MA from nbro_config.json
+nbroPath = path.join(dirPath, 'nbro_config.json')
+with open(nbroPath) as f:
+    nbroConfig = json.load(f)
+    lat = float(nbroConfig['latitude'])
+    lon = float(nbroConfig['longitude'])
+    overlock = f"lat={lat:.2f}&lng={lon:.2f}"
+
 resultsMe = getSun(mckay)
-overlock = "lat=42.33&lng=-71.64"
 resultsMa = getSun(overlock)
 
 lengthMe = resultsMe['day_length']
 lengthMa = resultsMa['day_length']
 
-
-if lengthMe >= lengthMa:
-    meLonger = True
-else:
-    meLonger = False
+meLonger = lengthMe >= lengthMa
 
 dif = abs(lengthMe - lengthMa)
 
 minDif = str((dif) // 60)
 secDif = str((dif) % 60)
-difString = str(minDif + ' min ' + secDif + ' sec')
+difString = f"{dif // 60} min {dif % 60} sec"
 
 # use stored credentials for client of the Google Drive API
-scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name(jsonFilePath, scope)
+scopes = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
+creds = Credentials.from_service_account_file(jsonFilePath, scopes=scopes)
 client = gspread.authorize(creds)
 
 # Find a workbook by name and open the first sheet
@@ -52,17 +65,18 @@ try:
     sheet = client.open('wx04849').get_worksheet(0)
     # 0 is the index of sheet in workbook
 except gspread.exceptions.APIError as ex:
-    print(ex)
-
-if meLonger:
-    sheet.update_cell(34,1,'Daylight in ME greater by')
-else:
-    sheet.update_cell(34,1,'Daylight in MA greater by')
-
-sheet.update_cell(34,2,difString)
+    logging.exception("Google Sheet didn't open for comp.py")
 
 stamp = datetime.now()
-sheet.update_cell(34,4,str(stamp))
-sheet.update_cell(34,5,'Called daily by comp.py')
+sheet.update(
+    range_name="A34:E34",
+    values=[[
+        'Daylight in ME greater by' if meLonger else 'Daylight in MA greater by',
+        difString,
+        '',
+        stamp.strftime("%Y-%m-%d %H:%M:%S"),
+        'Called daily by comp.py'
+    ]]
+)
 
-print('Updated daylight comparison ',str(stamp))
+logging.info(f"Updated daylight comparison {stamp}")
