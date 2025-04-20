995f0d477876ecad5e6fc11f70e52e1dcf5f3dff
Frank Capria <fcapria@gmail.com>
2025-04-20 13:58:22 -0400

    Improve logging structure in compare.py for clarity and traceability

--- compare.py
+++ compare.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Dark Sky calls
-
-import requests, json, time, gspread
+import requests, json, time, gspread, logging
 from datetime import datetime
-from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2.service_account import Credentials
 from open_api import openKey
+from wx_util import initLogging
 
 def gen_state(cur,last):
     d = cur - last
-    if d > -0.05 and d < 0.05:
+    if d > -0.1 and d < 0.1:
         msg = 'Steady'
     elif d > 0:
         msg = 'Rising'
     else:
         msg = 'Falling'
-    #print(msg)
     return msg
+
+initLogging("compare.py")
 
 openwx = 'https://api.openweathermap.org/data/3.0/onecall?lat='
 lLat = '44.4278389'
@@ -32,8 +32,6 @@ equalTemp = False
 
 lOpenCall = openwx + lLat + lLon + units + excludes + key
 nOpenCall = openwx + nLat + nLon + units + excludes + key
-
-# Define APIkey in dark_api.py and store in same directory as this script
 
 suffix = '°F'
 
@@ -62,11 +60,11 @@ try:
         
 except requests.exceptions as ex:
     greaterTemp = 'Unable to calculate temperature'
-    print(ex)
+    logging.error(f"OpenWeather API request failed: {ex}", exc_info=True)
     
 # use creds to create a client to interact with the Google Drive API
-scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('wx_secret.json', scope)
+scopes = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
+creds = Credentials.from_service_account_file('wx_secret.json', scopes=scopes)
 client = gspread.authorize(creds)
 
 # Find a workbook by name and open the first sheet
@@ -89,7 +87,11 @@ try:
     #cell = sheet.find('Current temperature')
     #sheet.update_cell(cell.row,cell.col+3,tempStatus)
     #sheet.update_cell(cell.row+5,cell.col+3,windStatus)
-    print('Comparisons complete',stamp)
+    logging.info("Google Sheet updated with:")
+    logging.info(f"  Temp Diff: {diffStr if not equalTemp else '-'}")
+    logging.info(f"  Stamp: {stamp}")
+
+    logging.info(f"compare.py complete: {stamp}")
 except gspread.exceptions.APIError as ex:
-    print("Sheet didn't open for compare.py ",ex)
+    logging.exception("Google Sheet didn't open for compare.py")
     
